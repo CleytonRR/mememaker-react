c8f5a05c402147c3b5fa9d316198036a5041aafb
cleytonRR <cleytonrodrigues69@live.com>
2020-06-24 23:35:48 -0300

    Created global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/Global';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </>
+  );
 }
 
 export default App;
